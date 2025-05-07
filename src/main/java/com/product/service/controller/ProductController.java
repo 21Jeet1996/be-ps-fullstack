d0efe8333bd51e7f04804cc4c3a7edbf34172325
21Jeet1996 <jaiswaramarjeet19@gmail.com>
2025-05-08 01:29:17 +0530

    implemented product service with crud

---
+++ src/main/java/com/product/service/controller/ProductController.java
@@ -0,0 +1,59 @@
+package com.product.service.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.product.service.model.Product;
+import com.product.service.service.ProductService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+	private final ProductService productService;
+
+	// @Autowired
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+
+	@GetMapping
+	public List<Product> getAllProducts() {
+		return productService.getAllProducts();
+	}
+
+	@GetMapping("/{id}")
+	public Optional<Product> getProductById(@PathVariable Long id) {
+		return productService.getProductById(id);
+	}
+
+	@PostMapping
+	public Product createProduct(@RequestBody Product product) {
+		return productService.saveProduct(product);
+	}
+
+	@PutMapping("/{id}")
+	public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+		product.setId(id);
+		return productService.saveProduct(product);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteProduct(@PathVariable Long id) {
+		productService.deleteProduct(id);
+	}
+}
---
+++ src/main/java/com/product/service/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.product.service.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.product.service.model.Product;
+import com.product.service.repo.ProductRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class ProductService {
+
+	private final ProductRepository productRepository;
+
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	public List<Product> getAllProducts() {
+		return productRepository.findAll();
+	}
+
+	public Optional<Product> getProductById(Long id) {
+		return productRepository.findById(id);
+	}
+
+	public Product saveProduct(Product product) {
+		return productRepository.save(product);
+	}
+
+	public void deleteProduct(Long id) {
+		productRepository.deleteById(id);
+	}
+}
